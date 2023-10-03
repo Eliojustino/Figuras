fe3365ccb20c3e4a7e08d05be2795b9d81bcde11
Eliojustino <126814395+Eliojustino@users.noreply.github.com>
2023-10-03 17:54:49 -0600

    Add files via upload

---
+++ figuras/src/main/java/Entity/Cuadrado.java
@@ -0,0 +1,14 @@
+package Entity;
+
+public class Cuadrado extends Figura {
+    private double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+}
---
+++ figuras/src/main/java/Entity/Figura.java
@@ -0,0 +1,9 @@
+package Entity;
+
+public abstract class Figura {
+    /**
+     * Creo un método abstracto llamado "calcularArea" que me regresará un 
+     * double  
+     */
+    public abstract double calcularArea();
+}
